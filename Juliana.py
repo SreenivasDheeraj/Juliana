2dc0cfe0eb5affc4109dc88e870fb92c1fa31e0a
Hrishikesh Vedantam <shikiikihs2@gmail.com>
2020-08-06 14:17:34 +0530

    small bug fix

--- Juliana.py
+++ Juliana.py
@@ -16,82 +16,82 @@ import random
 from discord import Member
 from discord.ext.commands import has_permissions
 
+if __name__ == '__main__':
+    ########################################## - Init - ###################################################
+    print("Init Bot...\n")
 
-########################################## - Init - ###################################################
-print("Init Bot...\n")
+    # Load Bot Data
+    configData = json.load(open('config.json'))
+    BotName = configData['bot_name']
+    TOKEN = configData['bot_token']
+    # TOKEN = 'Njc4Mjg2MTMxMDcwMjM4NzQw.XkgnJw.e_jqfJbM6acEt33Y_YiOqnfdpAM'
+    trigger = configData['bot_trigger']
 
-# Load Bot Data
-configData = json.load(open('config.json'))
-BotName = configData['bot_name']
-TOKEN = configData['bot_token']
-# TOKEN = 'Njc4Mjg2MTMxMDcwMjM4NzQw.XkgnJw.e_jqfJbM6acEt33Y_YiOqnfdpAM'
-trigger = configData['bot_trigger']
+    # Create Bot
+    client = Bot(command_prefix=trigger)
+    # channel = client.get_channel(678286131070238740)
 
-# Create Bot
-client = Bot(command_prefix=trigger)
-# channel = client.get_channel(678286131070238740)
-
-# Init Script Data
-# BotLibrary
-BotLibrary.configData = configData
-BotLibrary.client = client
-# FirebaseLibrary
-FirebaseLibrary.configData = configData
-FirebaseLibrary.DBInit()
+    # Init Script Data
+    # BotLibrary
+    BotLibrary.configData = configData
+    BotLibrary.client = client
+    # FirebaseLibrary
+    FirebaseLibrary.configData = configData
+    FirebaseLibrary.DBInit()
 
 
-# Load Custom Response Commands from directory
-BotLibrary.AddCustomResponses_FromPath(path=configData['trigger-response_path'])
-# Load Custom Response Commands from Cloud
-print("Adding Custom Responses from Cloud")
-BotLibrary.AddCustomResponses_FromData(FirebaseLibrary.GetAllTriggerResponses())
+    # Load Custom Response Commands from directory
+    BotLibrary.AddCustomResponses_FromPath(path=configData['trigger-response_path'])
+    # Load Custom Response Commands from Cloud
+    print("Adding Custom Responses from Cloud")
+    BotLibrary.AddCustomResponses_FromData(FirebaseLibrary.GetAllTriggerResponses())
 
-print("\nAll Custom Responses Loaded\n")
+    print("\nAll Custom Responses Loaded\n")
 
-print("\n\n")
-
-########################################## - Bot Events - #############################################
-# Bot Started Event
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print(str(client.user))
-    print('------')
     print("\n\n")
-    return
 
-########################################## - Basic Ping Commands - ####################################
-# Basic Commands
-BotLibrary.AddCommandsFromFile(client, 'Commands/Commands_Basic.py', globals())
+    ########################################## - Bot Events - #############################################
+    # Bot Started Event
+    @client.event
+    async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print(str(client.user))
+        print('------')
+        print("\n\n")
+        return
 
-########################################## - Admin Commands - ########################################
-BotLibrary.AddCommandsFromFile(client, 'Commands/Commands_Admin.py', globals())
-    
-########################################## - Further Commands - ########################################
-# 8 Ball Random Reponse
-BotLibrary.AddCommandsFromFile(client, 'Commands/Commands_8Ball.py', globals())
+    ########################################## - Basic Ping Commands - ####################################
+    # Basic Commands
+    BotLibrary.AddCommandsFromFile(client, 'Commands/Commands_Basic.py', globals())
 
-# Trigger-Response Commands
-BotLibrary.AddCommandsFromFile(client, 'Commands/Commands_Trigger-Response.py', globals())
+    ########################################## - Admin Commands - ########################################
+    BotLibrary.AddCommandsFromFile(client, 'Commands/Commands_Admin.py', globals())
 
-# Stack Overflow Commands
-BotLibrary.AddCommandsFromFile(client, 'Commands/Commands_StackOverflow.py', globals())
+    ########################################## - Further Commands - ########################################
+    # 8 Ball Random Reponse
+    BotLibrary.AddCommandsFromFile(client, 'Commands/Commands_8Ball.py', globals())
 
-########################################## - LOAD COGS - #####################################################
+    # Trigger-Response Commands
+    BotLibrary.AddCommandsFromFile(client, 'Commands/Commands_Trigger-Response.py', globals())
 
-for cog in os.listdir(os.path.abspath("Cogs")):
-    if cog.endswith(".py"):
-        try:
-            cog = f"Cogs.{cog.replace('.py', '')}"
-            client.load_extension(cog)
-            print("Loaded ", cog)
-        
-        except Exception as e:
-            print(f"{cog} cannot be loaded")
-            raise e
+    # Stack Overflow Commands
+    BotLibrary.AddCommandsFromFile(client, 'Commands/Commands_StackOverflow.py', globals())
 
-########################################## - RUN - #####################################################
-# Run the Client
-client.run(TOKEN)
+    ########################################## - LOAD COGS - #####################################################
+
+    for cog in os.listdir(os.path.abspath("Cogs")):
+        if cog.endswith(".py"):
+            try:
+                cog = f"Cogs.{cog.replace('.py', '')}"
+                client.load_extension(cog)
+                print("Loaded ", cog)
+
+            except Exception as e:
+                print(f"{cog} cannot be loaded")
+                raise e
+
+    ########################################## - RUN - #####################################################
+    # Run the Client
+    client.run(TOKEN)
